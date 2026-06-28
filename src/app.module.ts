d16426c0e00c9a6deabb0c40f972f6b0066aea5f
Ahemd Faysal <fr.abir.ahemd.faysal@gmail.com>
2026-06-28 19:54:29 +0600

    init the folder structre

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { SubscriptionModule } from './modules/subscription/subscription.module';
+import { MailModule } from './modules/mail/mail.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule, SubscriptionModule, MailModule],
   controllers: [AppController],
   providers: [AppService],
 })
